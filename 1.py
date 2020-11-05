1e861d3a76c7dd2794b582036fdfa47dadd0bac6
PK2080 <paulkbunter@gmail.com>
2020-11-05 16:47:27 +0000

    first lesson

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
 #test
+
+age = 5
+a = float(input("input age: "))
+b = "you are " 
+c = input("what is your name: ")
+print ("Hi",c,b,a, "years old")
+
+if a < 18:
+    print("your a child")
+elif a > 18 or a == 18: 
+    print("your an adult")
